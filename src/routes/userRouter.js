40ee304df3a6b3ed679f12ec23b14f521441205b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-18 16:19:36 +0200

    feat: GET / that returns all users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import prisma from "../db/prisma.js";
 
 const userRouter = new Router();
+
+userRouter.get("/", async (req, res) => {
+    return res.json(await prisma.user.findMany());
+});
 
 export default userRouter;
