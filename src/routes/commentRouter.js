75441c5d91f297f5438008f69840b7f4ef8d4b8d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-18 15:52:28 +0200

    feat: User, Post and Comment Routers

---
+++ src/routes/commentRouter.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const commentRouter = new Router();
+
+export default commentRouter;
---
+++ src/routes/userRouter.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const userRouter = new Router();
+
+export default userRouter;
