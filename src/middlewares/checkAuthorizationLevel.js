e47555ae7e6f68b58b44f9558ebaefb89d567157
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-21 20:09:14 +0200

    feat: Middleware to check id a logged in User has enough authorization

---
+++ src/middlewares/checkAuthorizationLevel.js
@@ -0,0 +1,19 @@
+const checkAuthorizationLevel = (idInParams, onlyBlogAuthor) => {
+    return (req, res, next) => {
+        if (
+            !onlyBlogAuthor &&
+            req.user.type === "normalUser" &&
+            parseInt(req.params[idInParams]) === parseInt(req.user.id)
+        ) {
+            return next();
+        } else if (req.user.type === "blogAuthor") {
+            return next();
+        }
+
+        return res.status(403).json({
+            errors: "You don't have proper authorization to delete the given User",
+        });
+    };
+};
+
+export default checkAuthorizationLevel;
