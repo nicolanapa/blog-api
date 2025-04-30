c69c5be006c5fa4b574d9914098c3563312ddf01
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-30 20:17:44 +0200

    feat: Use of cors with some options

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import process from "process";
+import cors from "cors";
 import userRouter from "./routes/userRouter.js";
 import postRouter from "./routes/postRouter.js";
 import commentRouter from "./routes/commentRouter.js";
@@ -7,10 +8,18 @@ import loginRouter from "./routes/loginRouter.js";
 
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 204,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.json({ "Available routes": ["/post", "/comment", "/user", "/login"] });
